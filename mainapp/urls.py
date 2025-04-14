ef99d0dcc1add4b819fd0fd832075898343be60f
Nyasha Nallen Tumba <nyashatumba@gmail.com>
2025-04-14 11:43:53 +0200

    2. Front-end Configuration
    Create urls for the mainapp
    Created views, templates and urls for dashboard, login & signup

---
+++ mainapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('userLogin', views.userLogin, name='userLogin'),
+    path('userSignup', views.userSignup, name='userSignup'),
+]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dashboard(request):
+    return render(request, 'index.html')
+
+def userSignup(request):
+    return render(request, 'signup.html')
+
+def userLogin(request):
+    return render(request, 'login.html')
